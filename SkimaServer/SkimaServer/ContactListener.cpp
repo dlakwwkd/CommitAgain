9dcf76ebee7dbcf31394044f874cec640f25bdf1
mong <ylmongte@gmail.com>
2014-11-15 17:36:38 +0900

    [S]contactListener 수정

--- SkimaServer/SkimaServer/ContactListener.cpp
+++ SkimaServer/SkimaServer/ContactListener.cpp
@@ -1,14 +1,26 @@
 #include "stdafx.h"
 #include "ContactListener.h"
 #include "Unit.h"
+#include "Hero.h"
 
 void ContactListener::BeginContact(b2Contact *contact)
 {
 	auto unitA = static_cast<Unit*>(contact->GetFixtureA()->GetBody()->GetUserData());
 	auto unitB = static_cast<Unit*>(contact->GetFixtureB()->GetBody()->GetUserData());
 
-	unitA->Crashed();
-	unitB->Crashed();
+	UnitType unitType = unitA->GetUnitType();
+	
+	switch (unitType)
+	{
+	case UNIT_HERO:
+		Hero* hero = dynamic_cast<Hero*>(unitA);
+		hero->Crashed();
+		break;
+
+	case UNIT_MISSILE:
+		Missile* missile = dynamic_cast<Missile*>(unitA);
+		hero->Crashed();
+	}
 }
 
 void ContactListener::EndContact(b2Contact* contact)
