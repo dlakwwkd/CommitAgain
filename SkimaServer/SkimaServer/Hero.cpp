fb7c8c11505eed7dd713ad62eb9d9b88908dee92
skdn159 <skdn159@naver.com>
2014-11-15 16:47:52 +0900

    [S] Hero, Player 수정

--- SkimaServer/SkimaServer/Hero.cpp
+++ SkimaServer/SkimaServer/Hero.cpp
@@ -18,6 +18,24 @@ Hero::Hero(int playerId, b2Vec2 pos)
 	m_Speed = 10.0f;
 	m_TargetPos = { 0, 0 };
 	m_PlayerID = playerId;
+
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(pos.x, pos.y);
+	m_Body = GGameManager->GetWolrd()->CreateBody(&bodyDef);
+
+	b2CircleShape circle;
+	circle.m_radius = 15.0f / PTM_RATIO;
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &circle;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+	fixtureDef.restitution = 0.7f;
+
+	m_Body->CreateFixture(&fixtureDef);
+	m_Body->SetUserData(this);
+
 	m_State = m_StandbyState = new StandbyState;
 	m_MovingState = new MovingState;
 	m_CrashedState = new CrashedState;
--- SkimaServer/SkimaServer/Unit.cpp
+++ SkimaServer/SkimaServer/Unit.cpp
@@ -9,23 +9,8 @@ Unit::Unit(int playerId, b2Vec2 pos)
 {
 	static int makeId = 0;
 	m_UnitID = ++makeId;
-	
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(pos.x, pos.y);
-	m_Body = GGameManager->GetWolrd()->CreateBody(&bodyDef);
+	m_Hp = 0;
 
-	b2CircleShape circle;
-	circle.m_radius = 15.0f/PTM_RATIO;
-
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &circle;
-	fixtureDef.density = 1.0f;
-	fixtureDef.friction = 0.3f;
-	fixtureDef.restitution = 0.7f;
-	
-	m_Body->CreateFixture(&fixtureDef);
-	m_Body->SetUserData(this);
 }
 
 Unit::Unit()
