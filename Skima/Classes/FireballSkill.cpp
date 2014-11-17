5010217055682a2ae55b413f69b9e2acc0ee3f76
sebin <tpqls5144@naver.com>
2014-11-17 22:32:12 +0900

    [C] 이게뭐야 왜 또 커밋이야!!!!

--- Skima/Classes/FireballSkill.cpp
+++ Skima/Classes/FireballSkill.cpp
@@ -1,12 +1,19 @@
 #include "pch.h"
 #include "FireballSkill.h"
+#include "FireballMissile.h"
 
 
 FireballSkill::FireballSkill()
 {
+	m_Missile = new FireballMissile;
 }
 
 
 FireballSkill::~FireballSkill()
 {
+}
+
+void FireballSkill::SkillCast(Point heroPos, Point targetPos)
+{
+	m_Missile->MissileCast(heroPos, targetPos);
 }
