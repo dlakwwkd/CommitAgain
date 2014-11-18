4d01651a2ede6e54778755806835a00e5ab2c627
dlakwwkd <dlakek@naver.com>
2014-11-18 11:15:23 +0900

    [C] 파이어볼 이미지 변경

--- Skima/Classes/FireballMissile.cpp
+++ Skima/Classes/FireballMissile.cpp
@@ -15,12 +15,12 @@ FireballMissile::~FireballMissile()
 
 void FireballMissile::MissileCast(Point createPos, Point targetPos)
 {
-	m_Particle = ParticleSystemQuad::create("Images/light.plist");
+	m_Particle = ParticleSystemQuad::create("Images/fire.plist");
 	m_Particle->setPosition(createPos);
 	auto distance = targetPos - createPos;
 	auto scala = sqrt(pow(distance.x, 2) + pow(distance.y, 2)) / 300;
 	auto action1 = MoveTo::create(scala, targetPos);
-	auto action2 = DelayTime::create(2.0);
+	auto action2 = DelayTime::create(1.0);
 	auto action3 = CallFunc::create(CC_CALLBACK_0(FireballMissile::MissileDelete, this));
 	auto action4 = Sequence::create(action1, action2, action3, NULL);
 	m_Particle->runAction(action4);
@@ -31,7 +31,7 @@ void FireballMissile::MissileCrash()
 {
 	m_Particle->stopAllActions();
 
-	auto action1 = DelayTime::create(2.0);
+	auto action1 = DelayTime::create(1.0);
 	auto action2 = CallFunc::create(CC_CALLBACK_0(FireballMissile::MissileDelete, this));
 	auto action3 = Sequence::create(action1, action2, NULL);
 	m_Particle->runAction(action3);
