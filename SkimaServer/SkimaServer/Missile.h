5550967b426d2e53bb99d488cd6287b22a5de344
skdn159 <skdn159@naver.com>
2014-11-21 17:48:15 +0900

    [S] missile move

--- SkimaServer/SkimaServer/Missile.h
+++ SkimaServer/SkimaServer/Missile.h
@@ -13,12 +13,14 @@ public:
 	void	SetMissileDamage(int missileDamage) { m_Damage = missileDamage; }
 	void	SetMissileSpeed(float missileSpeed) { m_Speed = missileSpeed; }
 	void	SetMissileLivetime(float missileLivetime) { m_Livetime = missileLivetime; }
+	void	SetMissileRange(float missileRange) { m_Range = missileRange; }
 
 	void	MissileShoot();
 	void	ConsumeLiveTime(float costTime);
 
 protected:
 	int		m_Damage;
+	float   m_Range;
 	float	m_Livetime;
 };
 
