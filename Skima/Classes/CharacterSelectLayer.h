73c60cfdfdc2ebbb91d9671ae907c6449850054d
skdn159 <skdn159@naver.com>
2014-12-05 02:52:04 +0900

    [C] characterSelect 진행중

---
+++ Skima/Classes/CharacterSelectLayer.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "PacketType.h"
+USING_NS_CC;
+
+typedef std::map<HeroType,Sprite*> FaceList;
+
+class CharacterSelectLayer : public Layer
+{
+public:
+    virtual bool init();
+    CREATE_FUNC(CharacterSelectLayer);
+
+private:
+    void MakeFaceSprite(const char* image, Vec2 pos, Vec2 scale, Vec2 anchor,HeroType hero);
+    void SetFaceProperty(MenuItemImage* img,Vec2 pos, Vec2 scale, Vec2 anchor);
+    void ChangeSelectedHero();
+
+    void ClickGameStart(); // 준비완료 레이어 추가??
+    void ClickGameExit();
+    void ClickMagician();
+    void ClickJupiter();
+
+private:
+    HeroType m_CurHero;
+    FaceList m_Facelist;
+
+    Sprite* m_MagicianSprite;
+    Sprite* m_JupiterSprite;
+};
+
