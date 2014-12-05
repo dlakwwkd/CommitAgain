c0d80004b69a2f04c355703f0fcfdd93f49524b6
skdn159 <skdn159@naver.com>
2014-12-06 00:36:08 +0900

    [C][R]레이어랑 리소스 추가
    cpp추가 했으므로 필터도 건드림
    근데 지금 내 깃이 이상한듯...

---
+++ Skima/Classes/WaitingLayer.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "WaitingLayer.h"
+#include "CharacterSelectLayer.h"
+
+
+bool WaitingLayer::init()
+{
+    if (!LayerColor::initWithColor(Color4B(0,0,0,0))) //검은색 세팅
+    {
+        return false;
+    }
+    this->setOpacity(300);
+
+    auto winSize = Director::getInstance()->getWinSize();
+
+    auto label = LabelTTF::create("Plz Wait", "Arial", 50);
+    label->setPosition(winSize.width/2, winSize.height/2);
+    this->addChild(label);
+
+    return true;
+}
+
---
+++ Skima/Classes/WaitingLayer.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+USING_NS_CC;
+
+class WaitingLayer : public LayerColor
+{
+public:
+    virtual bool init();
+    CREATE_FUNC(WaitingLayer);
+};
+
