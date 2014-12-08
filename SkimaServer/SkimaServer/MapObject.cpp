d3060af046da62a28e1d05e8f52b823d7ed218f4
mong <ylmongte@gmail.com>
2014-12-08 22:47:33 +0900

    [S]집감 커밋

--- SkimaServer/SkimaServer/MapObject.cpp
+++ SkimaServer/SkimaServer/MapObject.cpp
@@ -1,10 +1,32 @@
 #include "stdafx.h"
 #include "MapObject.h"
+#include "GameManager.h"
 
 
 MapObject::MapObject()
 {
-   
+    m_PlayerID = 0;
+    m_Hp = 10000;
+    m_Damage = 100;
+    m_Pos.x = 100;
+    m_Pos.y = 100;
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(m_Pos.x, m_Pos.y);
+    m_Body = GGameManager->GetWolrd()->CreateBody(&bodyDef);
+
+    b2CircleShape circle; //여기서부터 수정 해야 한다
+    circle.m_radius = REDUCE(DEF_SCALE);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &circle;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+    fixtureDef.restitution = 0.7f;
+
+    m_Body->CreateFixture(&fixtureDef);
+    m_Body->SetUserData(this);
 }
 
 
