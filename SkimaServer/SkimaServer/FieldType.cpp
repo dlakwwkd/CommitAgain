b5261fe09b993c8eaa0feaf2aecde640aab49a03
Song Kyung <skdn159@naver.com>
2014-12-28 17:53:15 +0900

    [S] Walldamage 구현중

--- SkimaServer/SkimaServer/FieldType.cpp
+++ SkimaServer/SkimaServer/FieldType.cpp
@@ -25,3 +25,23 @@ void FieldType::FieldDamage(const b2Vec2& targetPos, float scale, int damage)
 
     GGameManager->FieldDamage(m_Owner, &range, damage);
 }
+
+void FieldType::WallFieldDamage(const b2Vec2& targetPos, Walldirection direction, int damage)
+{
+    //switch(direction) 
+    //{}
+    b2Vec2 polygon[4];
+    polygon[0].Set(targetPos.x, targetPos.y); // 상수연산 설정
+    polygon[1].Set(targetPos.x, targetPos.y); 
+    polygon[2].Set(targetPos.x, targetPos.y);
+    polygon[3].Set(targetPos.x, targetPos.y);
+    int32 count = 4;
+
+    b2PolygonShape wall;
+    wall.Set(polygon, count);
+
+    
+     //GGameManager->FieldDamage(m_Owner, &range, damage); // &polyfonshape 넘겨줄것
+
+
+}
