0582e30e8ad5e4a6ac3aa40f1d80c488d7cfaa86
mong <ylmongte@gmail.com>
2014-12-28 23:23:39 +0900

    [C]item class 생성

---
+++ Skima/Classes/Item.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "Item.h"
+#include "GameManager.h"
+#include "ObjectLayer.h"
+
+
+Item::Item(Vec2 createPos, float scale)
+{
+    m_UnitType = UNIT_MOB;
+    m_MaxHp = 200.0f;
+    m_CurHp = m_MaxHp;
+    m_Speed = 100.0f;
+    m_CenterSprite->setPosition(createPos);
+    m_CenterSprite->setScale(scale);
+
+    m_RealSprite = Sprite::create("Images/Pea.png");
+    m_RealSprite->setScale(scale);
+    m_RealSprite->setAnchorPoint(Vec2(0, 0));
+    m_CenterSprite->addChild(m_RealSprite);
+}
+
+
+Item::~Item()
+{
+}
---
+++ Skima/Classes/Item.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "Unit.h"
+class Item :	public Unit
+{
+public:
+    Item(Vec2 createPos, float scale);
+    virtual ~Item();
+};
+
