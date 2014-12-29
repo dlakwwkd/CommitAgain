0584f41754a224d5dc444296be1fb9e9ad339293
sebin <tpqls5144@naver.com>
2014-12-30 08:28:29 +0900

    [C] PlayAgain할 시에 WaitingLayer곂치는 문제 해결

--- Skima/Classes/WaitingLayer.cpp
+++ Skima/Classes/WaitingLayer.cpp
@@ -3,6 +3,7 @@
 #include "RoomScene.h"
 
 #define GET_CONNECT_LABEL dynamic_cast<Label*>(this->getChildByName("ConnectLabel"))
+#define GET_ROOM_SCENE    dynamic_cast<RoomScene*>(this->getParent())
 
 bool WaitingLayer::init()
 {
@@ -15,7 +16,7 @@ bool WaitingLayer::init()
     auto winSize = Director::getInstance()->getWinSize();
 
     auto label = Label::create("다른 플레이어를 기다리는 중...", "Arial", 50);
-    label->setPosition(Vec2(winSize.width/2, winSize.height/2));  
+    label->setPosition(Vec2(winSize.width / 2, winSize.height * 3 / 4));
     this->addChild(label, 1, "ConnectLabel");
 
     return true;
@@ -38,10 +39,12 @@ void WaitingLayer::GameStart()
     auto action0 = CallFunc::create(CC_CALLBACK_0(Label::setString, label2, "3"));
     auto action1 = DelayTime::create(1.0f);
     auto action2 = CallFunc::create(CC_CALLBACK_0(Label::setString, label2, "2"));
-    auto action4 = CallFunc::create(CC_CALLBACK_0(Label::setString, label2, "1"));
-    auto action6 = CallFunc::create(CC_CALLBACK_0(RoomScene::GameStartComplete, dynamic_cast<RoomScene*>(this->getParent())));
+    auto action3 = CallFunc::create(CC_CALLBACK_0(Label::setString, label2, "1"));
+    auto action4 = CallFunc::create(CC_CALLBACK_0(RoomScene::GameStartComplete, GET_ROOM_SCENE));
+    auto action5 = CallFunc::create(CC_CALLBACK_0(Label::setString, label1, "다른 플레이어를 기다리는 중..."));
+    auto action6 = CallFunc::create(CC_CALLBACK_0(Label::setString, label2, " "));
     auto action = Sequence::create(action0, action1, action2,
-        action1, action4, action1, action6, NULL);
+        action1, action3, action1, action4, action5, action6, NULL);
 
     label2->runAction(action);
 }
