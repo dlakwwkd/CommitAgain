8df32063e3ce3f817f7de9b17c31e954ff09b897
mong <ylmongte@gmail.com>
2014-12-29 09:54:38 +0900

    [C]Buff class 생성

---
+++ Skima/Classes/Buff.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "Buff.h"
+#include "Hero.h"
+
+
+Buff::Buff()
+{
+}
+
+
+Buff::~Buff()
+{
+}
+
+void Buff::SkillCast(Vec2 heroPos, Vec2 targetPos)
+{
+
+}
+
+void Buff::SkillReady()
+{
+    auto arrow = m_Hero->GetArrow();
+    arrow->setVisible(true);
+}
+
+void Buff::SkillEnd()
+{
+    auto arrow = m_Hero->GetArrow();
+    arrow->setVisible(false);
+}
+
+void Buff::SkillCastForEnemy(Vec2 heroPos, Vec2 targetPos)
+{
+
+}
---
+++ Skima/Classes/Buff.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Skill.h"
+class Buff : public Skill
+{
+public:
+    Buff();
+    virtual ~Buff();
+    virtual void SkillCastForEnemy(Vec2 heroPos, Vec2 targetPos);
+    virtual void SkillCast(Vec2 heroPos, Vec2 targetPos);
+    virtual void SkillReady();
+    virtual void SkillEnd();
+};
+
